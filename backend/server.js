a3d7e278515c19b3ec42b336dbab0d1a50a0758d
Shawn Patterson <147225693+EngineerShawn@users.noreply.github.com>
2024-02-03 14:43:42 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const { PORT = 5001 } = process.env;
-const app = require("./app.js");
-const knex = require("./db/connection.js");
+const app = require("../src/App.js");
+const knex = require("../src/data/connection.js");
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 knex.migrate
   .latest()
