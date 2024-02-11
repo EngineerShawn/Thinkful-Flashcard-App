64c70462765283d39463282334261b84a0dd2684
Shawn Patterson <147225693+EngineerShawn@users.noreply.github.com>
2024-02-11 17:08:10 -0500

    Update connection.js

--- src/data/connection.js
+++ src/data/connection.js
@@ -1,5 +1,5 @@
 const environment = process.env.NODE_ENV || "production";
-const config = require("../../backend/knexfile")[environment];
+const config = require("../../backend/knexfile.js")[environment];
 const knex = require("knex")(config);
 
 module.exports = knex;
